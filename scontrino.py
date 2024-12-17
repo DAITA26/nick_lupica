6a376daf1c8360a870cf1d6deb7db15b092fb4e0
Niccolò L <niccololupica@gmail.com>
2024-12-17 09:50:56 +0100

    Second commit

--- scontrino.py
+++ scontrino.py
@@ -13,5 +13,10 @@ print(F"Per comprare {qty} di {item} devi pagare {tot}€. Grazie e arrivederci"
 # metodo_2 - calcolare il risultato e mettere la formula nella f strings
 print(F"Per comprare {qty} di {item} devi pagare {round(int(qty) * float(price), 2)}€. Grazie e arrivederci")
 
+# metodo_3 - fare conversioni all'inizio
+item_1 = input("Che cosa vuoi comprare? ")
+price_1 = float(input("Quanto costa? "))
+qty_1 = int(input("Quante ne compri? "))
+print(F"Per comprare {qty_1} di {item_1} devi pagare {round(qty_1 * price_1, 2)}€. Grazie e arrivederci")
 
 
