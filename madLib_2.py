fe8c69a9d00ded41c9ffc9768536ab834cec4a6c
Niccolò L <niccololupica@gmail.com>
2024-12-17 17:56:17 +0100

    modificato

--- madLib_2.py
+++ madLib_2.py
@@ -1,13 +1,13 @@
 print("Questo programmino serve a creare una piccola storia partendo da alcuni imput")
 
 # Chiedi input all'utente
-name = input("Dimmi un nome ").capitalize()
+name = input("Dimmi un nome ").strip().capitalize()
 if not name.isalpha():  # se il nome non contiene solo lettere riporta errore
     print("Errore: il nome può contenere solo lettere.")
-place = input("Dimmi il nome di un luogo ").lower()
-fruit= input("Dimmi un frutto ")
-number = float(input("Dimmi un numero "))
-number_2 = float(input("Dimmi un altro numero "))
+place = input("Dimmi il nome di un luogo ").strip().lower()
+fruit= input("Dimmi un frutto ").strip().lower()
+number = float(input("Dimmi un numero ").strip())
+number_2 = float(input("Dimmi un altro numero ").strip())
 
 # Stampa la storia
 print("\nEcco la tua storia")
