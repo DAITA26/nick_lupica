d889e6ae79a05014d37711da4d5ef702655e09ec
Niccolò L <niccololupica@gmail.com>
2024-12-30 01:11:54 +0100

    Second commit

--- giocoCartaForbiceSasso.py
+++ giocoCartaForbiceSasso.py
@@ -1,15 +1,21 @@
 import random
-
 contatore = 1
+
+print("Benvenuto al gioco di Carta, Forbice, Sasso!")
+print("Le regole sono semplici:")
+print("- Sasso batte Forbice")
+print("- Forbice batte Carta")
+print("- Carta batte Sasso")
+print("")
 
 
 while contatore <= 5:
     scelta_computer = random.randint(1, 3)
     # DEBUG
-    print(scelta_computer)
-    print(f"Sei al {contatore} Round! ")
-    print("Scegli tra sasso, carta e forbice")
-    mossa = input("Quale mossa scegli: ").strip().lower()
+    # print(scelta_computer)
+    print(f"Sei al {contatore} Round!")
+    print("Scegli tra sasso, carta e forbice.")
+    mossa = input("Quale mossa scegli? ").strip().lower()
     if not mossa.isalpha():
         print("Puoi inserire solo una tra le mosse carta, sasso e forbice")
         print("")
@@ -19,11 +25,11 @@ while contatore <= 5:
 
 #carta = 1, sasso = 2, forbice = 3
     if mossa == "carta" and scelta_computer == 2 or mossa == "sasso" and scelta_computer == 3 or mossa == "forbice" and scelta_computer == 1:
-        print(f"Hai vinto! Il tuo avversario aveva scelto {scelta_computer}")
+        print(f"Grande, hai vinto il round!")
         print("")
 
     elif mossa == "carta" and scelta_computer == 3 or mossa == "sasso" and scelta_computer == 1 or mossa == "forbice" and scelta_computer == 2:
-        print("Hai perso")
+        print("Mi spiace, hai perso!")
         print("")
     elif  mossa == "carta" and scelta_computer == 1 or mossa == "sasso" and scelta_computer == 2 or mossa == "forbice" and scelta_computer == 3:
         print(f"Hai pareggiato! Entrambi avete scelto {mossa}.")
