86305c6ab6e4a9b7ea060c731e8fa3ef55fbcc91
Niccolò L <niccololupica@gmail.com>
2025-01-07 12:01:25 +0100

    Second commit

--- giocoCartaForbiceSasso.py
+++ giocoCartaForbiceSasso.py
@@ -1,5 +1,7 @@
 import random
 contatore = 1
+pnt_usr = 0
+pnt_cpu = 0
 
 print("Benvenuto al gioco di Carta, Forbice, Sasso!")
 print("Le regole sono semplici:")
@@ -27,13 +29,28 @@ while contatore <= 5:
     if mossa == "carta" and scelta_computer == 2 or mossa == "sasso" and scelta_computer == 3 or mossa == "forbice" and scelta_computer == 1:
         print(f"Grande, hai vinto il round!")
         print("")
+        pnt_usr += 1
 
     elif mossa == "carta" and scelta_computer == 3 or mossa == "sasso" and scelta_computer == 1 or mossa == "forbice" and scelta_computer == 2:
         print("Mi spiace, hai perso!")
         print("")
+        pnt_cpu += 1
+
     elif  mossa == "carta" and scelta_computer == 1 or mossa == "sasso" and scelta_computer == 2 or mossa == "forbice" and scelta_computer == 3:
         print(f"Hai pareggiato! Entrambi avete scelto {mossa}.")
         print("")
     else:
         print("Non hai inserito una mossa valida! Riprova.")
         print("")
+
+
+else:
+    print("I round sono finiti. Scopriamo se hai vinto")
+    print(f"punt cpu = {pnt_cpu}")
+    print(f"punt usr = {pnt_usr}")
+    if pnt_cpu > pnt_usr:
+        print("Hai Persooo")
+    elif pnt_cpu == pnt_usr:
+        print("Pareggio, rigiochiamo?")
+    else:
+        print("Mi hai battuto!!!")
